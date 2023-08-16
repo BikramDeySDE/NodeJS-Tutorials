cef11ccfbb9c500ff33d61c7f9d39b926ded6081
BIKRAM DEY <133648903+BikramDeySDE@users.noreply.github.com>
2023-08-16 19:07:11 +0530

    Update npm.js

--- Node-6 (npm)/npm.js
+++ Node-6 (npm)/npm.js
@@ -1,1 +1,18 @@
 console.log("Let's learn npm");
+
+
+/*
+Different npm commands
+
+npm
+npm i
+npm install
+npm install {package-name}
+npm uninstall {package-name}
+
+npm install {package-name}@{version}  // version --> _._._
+npm install {package-name} --save-dev
+npm install {package-name} --global
+
+npm view {package-name} version
+*/
