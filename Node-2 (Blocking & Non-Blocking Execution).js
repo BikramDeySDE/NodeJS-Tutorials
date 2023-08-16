660eb364d9dcec4df70a88bdf8e5e8e2bba95193
BIKRAM DEY <133648903+BikramDeySDE@users.noreply.github.com>
2023-08-16 13:59:36 +0530

    Add files via upload

---
+++ Node-2 (Blocking & Non-Blocking Execution).js
@@ -0,0 +1,10 @@
+/*
+
+I)  Synchronous or Blocking Execution=>
+    - Line-by-line code execution
+
+II) Non-Synchronous or Non-Blocking Execution
+    - Line-by-line code execution NOT GURANTEED
+    - Callback will fire
+
+*/
